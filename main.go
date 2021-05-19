f01f9517b945226b2956d95599eee45c26a0950d
Fadhil Yaacob <mohamadfadhil.yaacob@onapp.com>
2021-05-19 13:02:41 +0800

    Add main.go

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+)
+
+// How to use:
+// firestore-meilisearch-sync -r -c firestore.json
+// firestore-meilisearch-sync -l -c firestore.json
+func main() {
+	// Read the firebase credentials
+}
