70a1681c09df5729bafb4681cf9fe102507f4fc8
Fadhil Yaacob <mohamadfadhil.yaacob@onapp.com>
2021-05-20 10:00:15 +0800

    improve cli command design

--- main.go
+++ main.go
@@ -3,9 +3,17 @@ package main
 import (
 )
 
-// How to use:
-// firestore-meilisearch-sync -r -c firestore.json
-// firestore-meilisearch-sync -l -c firestore.json
+// Custom Firebase credential file name
+// firestore-meilisearch-sync <collection name> -r -f firebase.json
+
+// Rebuild index:
+// firestore-meilisearch-sync <collection name> -r
+
+// Listen to changes:
+// firestore-meilisearch-sync <collection name> -l
+
+// Rebuild index & listen to changes:
+// firestore-meilisearch-sync <collection name> -r -l
 func main() {
 	// Read the firebase credentials
 }
